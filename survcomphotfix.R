60171ebe7cdb8826182308ff462b3c93e4bd18be
mlist <mlist@mpi-inf.mpg.de>
2016-11-15 00:12:26 +0100

    hotfix for installation issue in survcomp package

---
+++ survcomphotfix.R
@@ -0,0 +1,4 @@
+devtools::install_url("https://cran.r-project.org/src/contrib/Archive/SuppDists/SuppDists_1.1-9.2.tar.gz")
+source("https://bioconductor.org/biocLite.R")
+biocLite("survcomp")
+biocLite("genefu")
